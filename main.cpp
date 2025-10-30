0d9d346842241b211b82d29148e7811ae7c79e57
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2025-10-30 16:35:41 +0100

    Better main which also saves the output of the network.

--- main.cpp
+++ main.cpp
@@ -23,21 +23,25 @@ int main() {
     auto training_rule = pinv_flag ? "pinv" : "hebb"; 
 
     auto digit_patterns = load_digits(); 
+    
     net.train(digit_patterns, training_rule);
 
     for (size_t i = 0; i < 10; ++i) {
+        std::string number_path = "digits/N" + std::to_string(i);
+
         Pattern p = digit_patterns[i]; 
         
-        auto noisy_p = p.add_noise(0.5); 
-        noisy_p.display(); 
+        auto noisy_p = p.add_noise(0.5);
 
-        auto file = "digits/N" + std::to_string(i) + "_noisy.pbm"; 
-        noisy_p.to_pbm(file); 
+        noisy_p.display(); 
+        noisy_p.to_pbm(number_path + "_noisy.pbm"); 
 
         std::cout << "Initial energy: " << net.energy(noisy_p) << std::endl; 
 
         auto recalled_p = net.recall(noisy_p); 
+
         recalled_p.display();       
+        recalled_p.to_pbm(number_path + "_recalled.pbm");  
 
         std::cout << "Final energy: " << net.energy(recalled_p) << std::endl; 
         std::cout << "Error: " << recalled_p.error(p) * 100 << " %" << std::endl << std::endl; 
