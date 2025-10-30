b3859a43f7fae389bbd0adfa22fd3b61d91b5347
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2025-10-30 16:05:23 +0100

    Save noisy digits.

--- main.cpp
+++ main.cpp
@@ -25,20 +25,22 @@ int main() {
     auto digit_patterns = load_digits(); 
     net.train(digit_patterns, training_rule);
 
-    for (auto p : digit_patterns) {        
+    for (size_t i = 0; i < 10; ++i) {
+        Pattern p = digit_patterns[i]; 
+        
         auto noisy_p = p.add_noise(0.5); 
-        auto noisy_E = net.energy(noisy_p); 
-
-        std::cout << "Initial energy: " << noisy_E << std::endl; 
         noisy_p.display(); 
 
-        auto recalled_p = net.recall(noisy_p); 
-        auto recalled_E = net.energy(recalled_p); 
-        auto err = recalled_p.error(p); 
+        auto file = "digits/N" + std::to_string(i) + "_noisy.pbm"; 
+        noisy_p.to_pbm(file); 
 
-        recalled_p.display();         
-        std::cout << "Final energy: " << recalled_E << std::endl; 
-        std::cout << "Error: " << err * 100 << " %" << std::endl << std::endl; 
+        std::cout << "Initial energy: " << net.energy(noisy_p) << std::endl; 
+
+        auto recalled_p = net.recall(noisy_p); 
+        recalled_p.display();       
+
+        std::cout << "Final energy: " << net.energy(recalled_p) << std::endl; 
+        std::cout << "Error: " << recalled_p.error(p) * 100 << " %" << std::endl << std::endl; 
     }
     
     return 0; 
